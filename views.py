7fa7a584f3cf30647ad269fd1a845194818a1ca2
rijinrinu <136196445+rijinrinu@users.noreply.github.com>
2023-10-27 14:47:11 +0530

    Create views.py

---
+++ views.py
@@ -0,0 +1,5 @@
+from django. shortcuts import render from django. http import HTTP Response 
+from django. http import HttpResponseRedirect from django. core. files. Storage import FileSystemStorage import MySQLdb import web browser db.=MySQLdb. 
+Connect("localhost","root","","dB realistic") c=db.cursor() 
+login(request): 
+""" 
